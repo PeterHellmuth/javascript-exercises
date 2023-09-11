5e844b675d1c1fd2c0e6ba90db1a0ba56538255c
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 11:11:38 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if(firstNum < 0 || secondNum < 0){return "ERROR";}
+    if(typeof firstNum != "number" || typeof secondNum != "number"){return "ERROR";}
+    let smallNum = firstNum;
+    let bigNum = secondNum;
+    if(firstNum > secondNum){
+        smallNum = secondNum;
+        bigNum = firstNum;
+    }
+    if(firstNum === secondNum){
+        return firstNum+secondNum;
+    }
 
+    let sum=0;
+    for(i=0; i<=(bigNum-smallNum); i++){
+        sum += smallNum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
