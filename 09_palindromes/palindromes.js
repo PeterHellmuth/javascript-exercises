fb1bd10e608d8275dbd4645e791b2aa9398cb262
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 13:08:27 -0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (inputText) {
+    inputText = inputText.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedText = reverseString(inputText);
+    return reversedText == inputText;
 };
+
+function reverseString(inputText){
+    let stringArray = inputText.split('');
+    let reversedText = '';
+    for(i= inputText.length-1; i>= 0; i--){
+        reversedText += stringArray[i];
+    }
+    return reversedText;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
