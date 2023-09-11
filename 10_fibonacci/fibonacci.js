c7e34bd7eddc8671cfd3fd6870434e60876a263d
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 13:16:43 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [1,1];
+    if(num < 1){
+        return "OOPS";
+    }
+    if(num == 1 || num == 2){
+        return 1;
+    } else{
+        for(i = 3; i <= num; i++){
+            fib.push(fib[i-3]+fib[i-2]);
+        }
+    }
 
+    return fib[fib.length-1];
 };
 
 // Do not edit below this line
