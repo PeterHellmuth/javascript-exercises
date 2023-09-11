ead4f459fe403199add3d027c4d9e9f1c412accc
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 10:44:49 -0500

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let stringArray = inputString.split('');
+    let outputString = '';
+    for(i=inputString.length -1; i>= 0; i--){
+        outputString += stringArray[i];
+    }
+    return outputString;
 };
 
 // Do not edit below this line
