a69aa86ae36d28f1fba4e9f9e3893a4202a7920c
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 13:48:49 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let oldestIndex = -1;
+    for(i = 0; i < people.length; i++){
+        let age = 0;
+        if(people[i].hasOwnProperty("yearOfDeath")){
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else{
+            age = 2023 - people[i].yearOfBirth;
+        }
+
+        if(age > oldest){
+            oldest = age;
+            oldestIndex = i;
+        }
+    }
+    return people[oldestIndex];
 
 };
 
