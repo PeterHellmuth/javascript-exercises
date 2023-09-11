e14f6bb7ded175a621aa1ae0b681b6dcbca20a52
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 11:00:42 -0500

    Complete exercise four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+    for(i = 1; i < arguments.length; i++){
+        for(j=0; j< inputArray.length; j++){
+            if(inputArray[j] === arguments[i]){
+                inputArray.splice(j, 1);
+            }
+        }
+    }
+    return inputArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
