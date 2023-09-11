19660779d7b7d3d2b1c524fe1c4d0a9ecc7e913f
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 10:38:19 -0500

    Complete second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputText, inputIterations) {
+    if(inputIterations < 0){return "ERROR";}
+    let outputText  = '';
+    for(i=0; i< inputIterations; i++){
+        outputText += inputText;
+    }
+    return outputText;
 };
 
 // Do not edit below this line
