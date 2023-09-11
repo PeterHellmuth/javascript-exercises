12280acc7386f86508869fdad763ca0ec10390eb
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-11 12:32:14 -0500

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
-const add = function() {
+const add = function(firstNum, secondNum) {
+  if(typeof firstNum != "number" || typeof secondNum != "number"){return "ERROR";}
+	return firstNum + secondNum;
+};
+
+const subtract = function(firstNum, secondNum) {
+	if(typeof firstNum != "number" || typeof secondNum != "number"){return "ERROR";}
+  if(firstNum > secondNum){
+    return firstNum - secondNum;
+  } else{
+    return secondNum - firstNum;
+  }
+};
+
+const sum = function(numArray) {
+  let sum=0;
+  for(const num of numArray){
+    if(typeof num != "number"){
+      return "ERROR"
+    } else{
+      sum += num;
+    };
+  }
+  return sum;
+};
+
+const multiply = function(numArray) {
+  let result=1;
+  for(const num of numArray){
+    if(typeof num != "number"){
+      return "ERROR"
+    } else{
+      result *= num;
+    };
+  }
+  return result;
+};
+
+const power = function(firstNum, secondNum) {
+  if(typeof firstNum != "number" || typeof secondNum != "number"){return "ERROR";}
+  return firstNum**secondNum;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(firstNum,) {
+  if(typeof firstNum != "number"){return "ERROR";}
+  let result = 1;
+	for(i=1; i <= firstNum; i++){
+    result*= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
